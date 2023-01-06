1d392f67c0d2c3fac2934df908caf4cc1aef8f06
Jakub Olechowski <j.olechowski04@gmail.com>
2023-01-07 00:24:55 +0100

    Update env.ts

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -1,3 +1,4 @@
-export const API_URL = import.meta.env.VITE_API_URL as string
-export const BASE_URL = import.meta.env.VITE_BASE_URL as string
-export const APP_NAME = import.meta.env.VITE_APP_NAME as string
+export const APP_NAME = 'ServiceMenager'
+export const API_URL = 'https://api2.pigener.com/v1'
+export const API_URL_NO_VERSION = ''
+export const BASE_URL = ''
